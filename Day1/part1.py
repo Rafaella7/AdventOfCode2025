c9376a65d94ea3fce8f6935d7a66766654f3fb16
Rafella7 <rafa0704@hotmail.de>
2025-12-06 22:09:25 +0100

    Advent of Code Day 1 part 1

---
+++ Day1/part1.py
@@ -0,0 +1,25 @@
+dial = 50;
+num = 0
+
+with open('Day1/data1.txt', 'r') as f:
+    for line in f:
+        direction = line[0]
+        value = int(line[1:])
+        
+        if direction == "R":
+            dial += value
+        else:
+            dial -= value
+
+        if dial > 99:
+            dial = dial % 100
+        elif dial < 0:
+            dial = dial % 100
+        
+        if dial == 0:
+            num += 1
+
+
+print(num)
+
+
